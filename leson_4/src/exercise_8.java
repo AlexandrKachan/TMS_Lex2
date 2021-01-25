c8f535e2d606337f24348f3378d297095bb577d4
Alexandr <kachan.alexandr@gmail.com>
2021-01-25 18:28:18 +0300

    Задачи 8 доделаны рубли от 11 до 20

--- leson_4/src/exercise_8.java
+++ leson_4/src/exercise_8.java
@@ -1,45 +1,45 @@
 import java.util.Random;
 
 public class exercise_8 {
-//Имеется целое число  Random rand = new Random(); int x = rand.nextInt();
-public static void main(String[] args) {
+//Имеется целое число  Random rand = new Random(); int x = rand.nextInt(); Это число - количество денег в рублях.
+// Вывести это число, добавив к нему слово "рублей" в правильном падеже.
 
-    Random rand = new Random();
-    //int x = rand.nextInt();
-    int x = rand.nextInt(100000);
-    int lastX=0;
-    if (x<0) lastX = (x % 10)*-1;
-    else {
-        lastX = (x % 10) ;
+    public static void main(String[] args) {
+
+        Random rand = new Random();
+        //int x = rand.nextInt();
+        int x = rand.nextInt(100000);
+        int xPlus = 0;
+        if (x < 0) {
+            xPlus = x * -1;
+        } else xPlus=x;
+        int lastX;
+        int lastY;
+
+        lastX = (xPlus % 10);
+        lastY = (xPlus % 100);
+
+        System.out.println(x);
+        System.out.println(xPlus);
+        System.out.println(lastX);
+        System.out.println(lastY);
+
+        if (lastY >=11 & lastY <= 20) {
+            System.out.println( x + " рублей");
+        }else if ((lastX == 1)) {
+            System.out.println(x + " рубль");
+        } else if ((lastX >= 2) & (lastX <= 4)) {
+            System.out.println(x + " рубля");
+        } else if (lastX == 0) {
+            System.out.println(x + " рублей");
+        } else if (lastX >= 5) {
+            System.out.println(x + " рублей");
+
+        }
+
+
+
     }
-
-    System.out.println( x);
-  //  System.out.println( y);
-    System.out.println(lastX);
-
-    if ((lastX == 1)) {
-        System.out.println(x+" рубль");
-    } else if ((lastX >=2) &(lastX<=4)){
-        System.out.println(x+" рубля");
-    } else if  (lastX==0){
-        System.out.println(x+" рублей");
-    } else if (lastX >=5) {
-        System.out.println(x+" рублей");
-
-    }
-
-
-
-
-
-}
-
-
-
-
-
-
-
 
 
 }
